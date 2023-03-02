9a886768dd07ec79e9e37e17d2070225eda730c9
hanghanghappy <chc063@ucsd.edu>
2023-03-02 15:28:24 -0800

    Added styling for the dialog form

--- javascript/styledblog.js
+++ javascript/styledblog.js
@@ -42,6 +42,22 @@ function create_post(){
     dialog_area.appendChild(clone);
     const dialog_opener = document.getElementById("custom_dialog");
     dialog_opener.show();
+    
+    // Styling for the dialog form
+    dialog_opener.style.width = "400px";
+    dialog_opener.style.border = "2px solid gray";
+    dialog_opener.style.borderRadius = "5px";
+
+    const inputElements = dialog_opener.querySelectorAll("input, textarea");
+    inputElements.forEach((inputElement) => {
+        inputElement.style.display = "block";
+        inputElement.style.width = "100%";
+        inputElement.style.marginBottom = "10px";
+        inputElement.style.padding = "5px";
+        inputElement.style.fontSize = "15px";
+        inputElement.style.border = "1px solid gray";
+        inputElement.style.borderRadius = "5px";
+      });
 
     const save_button = document.getElementById("save");
     const cancel_button = document.getElementById("cancel");
